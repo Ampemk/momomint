2938b8c27149c10c25f10638ba51c454a748ea80
Kwaku Anansi <ampemk@gmail.com>
2022-06-24 19:29:50 +0000

    added payment recieved.pwd

--- app/Jobs/ProcessWebhookJob.php
+++ app/Jobs/ProcessWebhookJob.php
@@ -26,9 +26,9 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
     public function handle()
     {
         $payload = $this->webhookCall->payload;
-        $value =(object) $payload['entry'][0]['changes'][0]['value'];
+        $value = (object) $payload['entry'][0]['changes'][0]['value'];
         $messages = $value->messages[0];
-	Log::debug($messages);
+        Log::debug($messages);
         if ($messages['type'] !== 'text') {
             return false;
         }
@@ -36,9 +36,9 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
         $contact = $value->contacts[0];
         $message = $messages['text']['body'];
         $phone_number = $contact['wa_id'];
-	Log::debug($phone_number);
-        $account = Account::where('account_number','=', $phone_number)->first();
-	Log::debug($account);
+        Log::debug($phone_number);
+        $account = Account::where('account_number', '=', $phone_number)->first();
+        Log::debug($account);
         if (Str::of($message)->startsWith('Payment made for')) {
             $this->startWithPayment($message, $phone_number, $account);
         }
@@ -46,6 +46,60 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
         if (Str::of($message)->startsWith('Your payment of')) {
             $this->startWithYour($message, $phone_number, $account);
         }
+
+        if (Str::of($message)->startsWith('Payment received for')) {
+            $this->startWithPaymentRecieved($message, $phone_number, $account);
+        }
+    }
+
+    public function startWithPaymentRecieved($message, $phone_number, $account)
+    {
+
+        preg_match_all('/([0-9]+\.[0-9]+)/', $message, $matches);
+
+        $getCurrentBalancePosition = strpos($message, 'Current');
+        $newString = substr($message, 0, $getCurrentBalancePosition);
+        $newStringArray = explode(' from ', $newString);
+
+        preg_match_all('!\d+!', $newStringArray[1], $hasNumber);
+
+        if (!empty($hasNumber[0][0])) {
+            $toNumber = $hasNumber[0][0];
+
+            $nameOrCompany = preg_replace('/[0-9]+/', '', $newStringArray[1]);
+            $nameOrCompany = str_replace('()', '', $nameOrCompany);
+        } else {
+            $nameOrCompany = $newStringArray[1];
+            $toNumber = $phone_number;
+        }
+
+        $amount = $matches[0][0];
+        $current_balance = $matches[0][1];
+        $available_balance = $matches[0][2];
+        $fees_charged = $matches[0][3];
+        if (count($matches) === 4) {
+            $tax_charged = $matches[0][4];
+        } else {
+            $tax_charged = 0.00;
+        }
+        $transaction_id = $this->getTransactionID($message, 'Transaction ID');
+        $reference = $this->getReference($message, 1);
+
+        $momo_statement = new MomoStatement();
+        $momo_statement->from_name = $account->user->first_name . ' ' . $account->user->last_name;
+        $momo_statement->amount = $amount;
+        $momo_statement->to_no = $toNumber;
+        $momo_statement->to_name = $nameOrCompany;
+        $momo_statement->ref = $reference;
+        $momo_statement->fees = $fees_charged + $tax_charged;
+        $momo_statement->f_id = $transaction_id;
+        $momo_statement->bal_before = $current_balance;
+        $momo_statement->bal_after = $available_balance;
+        $momo_statement->from_no = $phone_number;
+        $momo_statement->account_id = $account->id;
+        $momo_statement->user_id = $account->user->id;
+        $momo_statement->transaction_date = date('Y-m-d H:i:s');
+        $momo_statement->save();
     }
 
     public function startWithPayment($message, $phone_number, $account)
@@ -77,7 +131,7 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
         } else {
             $tax_charged = 0.00;
         }
-        $transaction_id = $this->getTransactionID($message,'Transaction ID');
+        $transaction_id = $this->getTransactionID($message, 'Transaction ID');
         $reference = $this->getReference($message, 1);
 
         $momo_statement = new MomoStatement();
@@ -119,7 +173,7 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
 
         $amount = $matches[0][0];
         $current_balance = $matches[0][1];
-       // $available_balance = $matches[0][2];
+        // $available_balance = $matches[0][2];
         $fees_charged = $matches[0][2];
         if (count($matches) === 3) {
             $tax_charged = $matches[0][3];
@@ -127,11 +181,11 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
             $tax_charged = 0.00;
         }
         $reference = $this->getReference($message, 2);
-        $transaction_id = $this->getTransactionID($message,'Transaction Id');
+        $transaction_id = $this->getTransactionID($message, 'Transaction Id');
 
         $momo_statement = new MomoStatement();
         $momo_statement->from_name = $account->user->first_name . ' ' . $account->user->last_name;
-	$momo_statement->amount = $amount;
+        $momo_statement->amount = $amount;
         $momo_statement->to_no = $toNumber;
         $momo_statement->to_name = $nameOrCompany;
         $momo_statement->ref = $reference;
@@ -168,7 +222,7 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
         return str_replace('.', '', $reference_string);
     }
 
-    public function getTransactionID($text,$str)
+    public function getTransactionID($text, $str)
     {
         $transaction_id_position = strpos($text, $str) + 15;
         $transaction_id = substr($text, $transaction_id_position, 12);
